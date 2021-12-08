65d4220765955c9908c1a1e551af02b87d30ed0a
Marco Parra <marcoparra311@gmail.com>
2021-12-08 19:55:31 -0400

    Preserve check value using localStorage and window.matchmedia method

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -10,9 +10,16 @@ const setLight = () => {
   localStorage.setItem('theme', 'light');
   document.documentElement.setAttribute('data-theme', 'light');
 }
+const storedTheme = localStorage.getItem("theme");
+const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+const defaultDark = storedTheme === "dark" || (storedTheme === null && prefersDark);
 
 const handleTheme: ChangeEventHandler<HTMLInputElement> = (e) => {
   e.target.checked ? setDark() : setLight();
+}
+
+if (defaultDark) {
+  setDark();
 }
 
 const DarkMode = () => {
@@ -20,7 +27,12 @@ const DarkMode = () => {
     <div className='toggle-theme-wrapper'>
       <span>☀️</span>
       <label className='toggle-theme' htmlFor='checkbox'>
-        <input type="checkbox" id="checkbox" onChange={handleTheme} />
+        <input 
+          type="checkbox" 
+          id="checkbox" 
+          onChange={handleTheme} 
+          defaultChecked={defaultDark}
+        />
         <div className='slider round'> </div>
       </label>
       <span>🌒</span>
